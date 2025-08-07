8653b06de83546ca49c9791aacd6a3d57e27ff46
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2025-08-07 08:32:37 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { UsuarioComponent } from "./usuario/usuario.component";
+
+@Component({
+  selector: 'app-raiz',
+  standalone: true,
+  imports: [EncabezadoComponent, UsuarioComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {}
---
+++ src/app/usuario/usuario.component.ts
@@ -0,0 +1,24 @@
+import { Component, computed, signal } from '@angular/core';
+
+import { USUARIOS_FALSOS } from '../usuarios-falsos';
+const indiceAleatorio = Math.floor(Math.random() * USUARIOS_FALSOS.length);
+
+@Component({
+  selector: 'app-usuario',
+  standalone: true,
+  templateUrl: './usuario.component.html',
+  styleUrl: './usuario.component.css'
+})
+export class UsuarioComponent {
+  usuarioSeleccionado = signal(USUARIOS_FALSOS[indiceAleatorio]);
+  rutaImagen = computed(() => 'assets/usuarios/' + this.usuarioSeleccionado().avatar);
+
+  // get rutaImagen() {
+  //   return 'assets/usuarios/' + this.usuarioSeleccionado.avatar;
+  // }
+
+  alSeleccionarUsuario() {
+    const indiceAleatorio = Math.floor(Math.random() * USUARIOS_FALSOS.length);
+    this.usuarioSeleccionado.set(USUARIOS_FALSOS[indiceAleatorio]);
+  }
+}
