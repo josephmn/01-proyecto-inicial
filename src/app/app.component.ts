82a0ee4a42e07de86bc0259583f69bdeeb4f0899
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2025-08-07 12:05:25 -0500

    usando ngFor y ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { EncabezadoComponent } from './encabezado/encabezado.component';
 import { UsuarioComponent } from "./usuario/usuario.component";
 import { USUARIOS_FALSOS } from './usuarios-falsos';
 import { TareasComponent } from "./tareas/tareas.component";
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-raiz',
   standalone: true,
-  imports: [EncabezadoComponent, UsuarioComponent, TareasComponent],
+  imports: [EncabezadoComponent, UsuarioComponent, TareasComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
