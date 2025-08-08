a1f19d50f21e33011674ae2ffe4a9d9899ed578f
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2025-08-08 10:43:55 -0500

    dia 5 - directivas y vinculacion bidireccional

--- src/app/tareas/nueva-tarea/nueva-tarea.component.ts
+++ src/app/tareas/nueva-tarea/nueva-tarea.component.ts
@@ -1,14 +1,18 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nueva-tarea',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './nueva-tarea.component.html',
   styleUrl: './nueva-tarea.component.css'
 })
 export class NuevaTareaComponent {
   @Output() cacelar = new EventEmitter<void>();
+  tituloIngresado = '';
+  resumenIngresado = '';
+  fechaIngresada = '';
   
   alCancelar() {
     this.cacelar.emit();
